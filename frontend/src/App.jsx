02bbbfdf1fb03c9d022e93d579419ae3d8300387
rjn991 <ranjanht991@gmail.com>
2025-01-03 09:50:36 +0530

    Initial frontend push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      <p>Hai</p>
+      <Button className="m-5 px-10">Click me</Button>
+    </>
+  )
+}
+
+export default App
